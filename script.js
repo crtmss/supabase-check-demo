91a538dfdc0eeb6351f90bfa3f9a7ceea98e4f04
crtmss <nicklol225@gmail.com>
2025-04-19 08:42:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 const SUPABASE_URL = "https://pcdveqprfopaofcjkady.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBjZHZlcXByZm9wYW9mY2prYWR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUwNDMyMDksImV4cCI6MjA2MDYxOTIwOX0.YYffphzHl9CtG6L9XpEBLgFE9WfYSq_F-RT3cg10d_k";
 
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 async function checkConnection() {
   try {
-    const { data, error } = await supabase.from("lobbies").select("*").limit(1);
+    const { data, error } = await client.from("lobbies").select("*").limit(1);
     const statusEl = document.getElementById("status");
 
     console.log("Data:", data);
